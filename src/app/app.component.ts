8c627741f25c88003fef75fe481d01ebdd22c51b
Josileudo <josileudorodrigues75@gmail.com>
2021-10-14 11:32:52 -0300

    created convertion from binary

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.less']
 })
-export class AppComponent {
-  title = 'app works!';
+export class AppComponent implements OnInit {
+
+  decimalValue: number = 0;
+  binaryValue: string;
+  inputValue: any;
+
+  constructor(){}
+
+  binaryInput(value: string ) {
+    return parseInt((value ),2)
+  }
+
+  formatInput() {
+    setTimeout(() => {
+      this.binaryValue = this.binaryValue.replace(/[^0-1]/gi,'')
+    } , 1)
+  }
+
+  convertValues(value: string){
+    const result = this.binaryInput(value)
+    this.decimalValue = result
+
+  }
+
+
+  ngOnInit(){
+
+  }
 
 }
